0ea5243d8301570ac17c0ce5fef48bcd25d98599
Oreskov Stojance <133867596+oreskovs@users.noreply.github.com>
2025-04-22 08:33:58 +0200

    Add files via upload

---
+++ Getting user input/Getting user input/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GettingUserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name:");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age:");
+            string age = Console.ReadLine();
+            Console.WriteLine("Hello " + name + " you are " + age + " years old!");
+
+
+            Console.ReadLine();
+        }
+    }
+}
