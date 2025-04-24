722d74ccad81e87f1427a85991c53a62a6327c89
Oreskov Stojance <133867596+oreskovs@users.noreply.github.com>
2025-04-24 08:02:35 +0200

    Add files via upload

---
+++ Building an Exponent Method/Building an Exponent Method/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExponentMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(GetPow(2, 3));
+
+
+            Console.ReadLine();
+        }
+
+        static int GetPow(int baseNum, int powNum)
+        {
+            int result = 1;
+
+            for (int i = 0; i < powNum; i++)
+            {
+                result = result * baseNum;
+            }
+
+
+            return result;
+        }
+    }
+}
