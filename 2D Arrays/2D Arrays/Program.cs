3fbe254795febcf3b2ace9065ddfe731d90f4de4
Oreskov Stojance <133867596+oreskovs@users.noreply.github.com>
2025-04-25 18:07:00 +0200

    Add files via upload

---
+++ 2D Arrays/2D Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TwoDArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] numberGrid = {
+                {1,2 },
+                {3,4 },
+                {5,6 }
+            };
+
+            int[,] myArray = new int[2, 3];
+
+            Console.WriteLine(numberGrid[1,1]);
+
+
+            Console.ReadLine();
+        }
+    }
+}
