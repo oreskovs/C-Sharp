191310c8dd1054f0be35153c5fac7c2182c9e227
Oreskov Stojance <133867596+oreskovs@users.noreply.github.com>
2025-04-26 13:04:23 +0200

    Add files via upload

---
+++ Guessing Game/Guessing Game/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string secretWord = "giraffe";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 5;
+            bool outOfGuesses = false;
+
+            while (guess != secretWord && !outOfGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+               }
+            if (outOfGuesses)
+            {
+                Console.WriteLine("You loose!");
+            }
+            else
+            {
+                Console.WriteLine("You win.\nThe secret word was " + guess );
+            }
+
+
+
+                Console.ReadLine();
+        }
+    }
+}
