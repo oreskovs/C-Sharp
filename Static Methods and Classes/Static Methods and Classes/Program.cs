1002212a3a9da990e417798044443736ac61f290
Oreskov Stojance <133867596+oreskovs@users.noreply.github.com>
2025-04-30 18:11:27 +0200

    Add files via upload

---
+++ Static Methods and Classes/Static Methods and Classes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Static_Methods_and_Classes;
+
+namespace StaticMethodsandClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            UsefulTools.SayHi("mIKE");
+
+            Console.ReadLine();
+        }
+    }
+}
