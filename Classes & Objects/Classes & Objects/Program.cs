45db1aae1267f93ee64dda8ea7fc902777e0dc93
Oreskov Stojance <133867596+oreskovs@users.noreply.github.com>
2025-05-02 12:01:23 +0200

    Add files via upload

---
+++ Classes & Objects/Classes & Objects/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Classes___Objects;
+
+namespace ClassesandObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book book1 = new Book();
+            book1.title = "Harry Potter";
+            book1.author = "JK Rowling";
+            book1.pages = 400;
+
+            Console.WriteLine(book1.title);
+            Console.WriteLine(book1.author);
+            Console.WriteLine(book1.pages);
+
+            Book book2 = new Book();
+            book2.title = "Lord Of the Rings";
+            book2.author = "Tolkein";
+            book2.pages = 700;
+
+            Console.WriteLine(book2.title);
+            Console.WriteLine(book2.author);
+            Console.WriteLine(book2.pages);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
