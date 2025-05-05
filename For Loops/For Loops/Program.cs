857bd2ead40fa23c7acc5a361aad3f318a78b6c1
Oreskov Stojance <133867596+oreskovs@users.noreply.github.com>
2025-05-05 08:00:51 +0200

    Add files via upload

---
+++ For Loops/For Loops/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumbers = { 5, 7, 18, 24, 28, 33, 77 };
+
+            for (int i = 0; i < luckyNumbers.Length; i++)
+            {
+                Console.WriteLine(luckyNumbers[i]);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
