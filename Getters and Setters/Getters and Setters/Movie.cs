cc0540a096d7b51fee19a4a48efe19231e91ed9b
Oreskov Stojance <133867596+oreskovs@users.noreply.github.com>
2025-05-08 17:54:50 +0200

    Add files via upload

---
+++ Getters and Setters/Getters and Setters/Movie.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getters_and_Setters
+{
+    class Movie
+    {
+        public string title;
+        public string author;
+        private string rating;
+
+        public Movie(string aTitle, string aAuthor, string aRating)
+        {
+            title = aTitle;
+            author = aAuthor;
+            Rating = aRating;
+        }
+
+        public string Rating
+        {
+            get
+            {
+                return rating;
+            }
+            set
+            {
+                if(value == "G" || value == "PG" || value == "PG-13" || value == "R" || value == "NR")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR";
+                }
+            }
+        }
+
+
+
+
+    }
+}
