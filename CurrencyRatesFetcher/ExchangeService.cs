9decb74c6785995f5b7a199a61a6c416d8a6ae30
Oreskov Stojance <133867596+oreskovs@users.noreply.github.com>
2025-07-16 13:57:29 +0200

    Add files via upload

---
+++ CurrencyRatesFetcher/ExchangeService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CurrencyRatesFetcher
+{
+    internal class ExchangeService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ExchangeService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<string> GetRatesAsync(string currency)
+        {
+            try
+            {
+                string url = $"https://api.exchangerate.host/latest?base=EUR";
+
+                var response = await _httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string json = await response.Content.ReadAsStringAsync();
+                ExcangeRates rates = JsonSerializer.Deserialize<ExcangeRates>(json);
+
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine($"\nExchange rates for {rates.Base} on {rates.Date}:");
+
+                foreach (var pair in rates.Rates.Take(10))
+                {
+                    sb.AppendLine($"{pair.Key}: {pair.Value}");
+                }
+
+                return sb.ToString();
+
+
+            }
+            catch (Exception err)
+            {
+                return $"Failed to fetch rates {err.Message}";
+            }
+        }
+    }
+}
---
+++ CurrencyRatesFetcher/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+using CurrencyRatesFetcher;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ExchangeService service = new ExchangeService();
+
+            Console.WriteLine("Enter base currency (e.g., EUR, USD):");
+            string currency = Console.ReadLine().ToUpper();
+
+            string result = await service.GetRatesAsync(currency);
+            Console.WriteLine(result);
+        }
+    }
+}
